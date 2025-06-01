fe4d8665a031be775d9ecc385311741e71df0e8a
lisap <poehllisa@gmail.com>
2025-06-01 15:20:38 +0200

    Logik geändert: CreamDetailActivity, DropDetailActivity, InhalationDetailActivity
    damit diese beim Ausführen der App zumindest angezeigt werden

--- app/src/main/java/at/fhj/tessaimrich/InhalationDetailActivity.java
+++ app/src/main/java/at/fhj/tessaimrich/InhalationDetailActivity.java
@@ -1,6 +1,8 @@
 package at.fhj.tessaimrich;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class InhalationDetailActivity extends AppCompatActivity {
 
+public class InhalationDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_inhalation_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        ImageButton btnHome = findViewById(R.id.btnHome);
+        if (btnHome != null) {
+            btnHome.setOnClickListener(v -> {
+                Intent intent = new Intent(this, CategoryActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                finish();
+            });
+        }
     }
 }
