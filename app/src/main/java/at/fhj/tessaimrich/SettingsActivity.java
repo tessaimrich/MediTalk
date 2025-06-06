126a33deb710cd24db0c07a3064e185c06eeb216
lisap <poehllisa@gmail.com>
2025-06-06 15:16:38 +0200

    Kommentar geändert in activity_settings.xml, in SettingsActivity nur was probiert

--- app/src/main/java/at/fhj/tessaimrich/SettingsActivity.java
+++ app/src/main/java/at/fhj/tessaimrich/SettingsActivity.java
@@ -7,8 +7,8 @@ public class SettingsActivity extends BaseDrawerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // 1) Drawer-Layout ist bereits in BaseDrawerActivity gesetzt.
-        // 2) Wir blasen nur noch unser settings-Layout in den content_frame:
+        // Drawer-Layout ist bereits in BaseDrawerActivity gesetzt.
+        // Wir blasen nur noch unser settings-Layout in den content_frame:
         getLayoutInflater().inflate(
                 R.layout.activity_settings,
                 findViewById(R.id.content_frame),
